dd4207f1e5f49a5ebb337fce8918d3a93b66ec8c
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:27:26 -0500

    Suma de dos numeros

--- nachole.java
+++ nachole.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 public class nachole {
     public static void main(String[] args){
-        System.out.print("Sumemos 2 numeros");
+        System.out.println("Sumemos 2 numeros");
         int n1 = 0, n2 = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese un numero (1)");
+        System.out.println("Ingrese un numero (1): ");
         n1 = sc.nextInt();
-        System.out.println("Ingrese un numero (2)");
+        System.out.println("Ingrese un numero (2): ");
         n2 = sc.nextInt();
-        System.out.println("La suma es"+ (n1 + n2));
+        System.out.println("La suma es: "+ SumadeNumeros(n1, n2));
 
+    }
+
+    public static int SumadeNumeros(int n1, int n2){
+        return n1 + n2;
     }
 
 }
